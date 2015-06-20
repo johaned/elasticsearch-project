0673f38ab4e4f5b41bdd769cb2dac818fa8300e8
Johaned <johan.tique@codescrum.com>
2015-06-20 12:49:45 -0500

    add another strategy for indexing

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,14 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
+client = Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
+
+# Book.__elasticsearch__.client.indices.delete index: Book.index_name rescue nil
 
 unless Book.__elasticsearch__.index_exists?
   Book.__elasticsearch__.create_index! force: true
   Book.import
 end
+
+# client.indices.delete index: Book.index_name rescue nil
+# client.bulk index: Book.index_name,
+#             type:  'book',
+#             body:  Book.all.map { |b| { index: { _id: b.id.to_s, data: b.attributes } } },
+#             refresh: true
